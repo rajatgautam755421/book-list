2d7b6bf4a27ca2a7c28bf126246a40dfb050bf3f
rajatgautam755421 <83450756+rajatgautam755421@users.noreply.github.com>
2021-05-02 15:30:26 +0545

    Add files via upload

--- server.js
+++ server.js
@@ -1,100 +1,36 @@
-const express = require("express");
-const { connect } = require("mongoose");
-const Post = require("./models/post");
-const bodyParser = require("body-parser");
-const path = require("path");
+const { request } = require('express');
+const express = require ('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const { connect } = require("mongoose");
+const authRoute = require('./routes/auth');
+const bookRoute = require('./routes/bookroutes')
+const bodyParser = require('body-parser')
+const postRoute = require('./routes/path')
+const verify = require('./routes/verifyToken');
+port = 3000;
+require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-//GET request
-app.get('/',(req,res)=>{
-  res.send({msg : 'Book List'})
-});
+//Routes
+app.use('/user',authRoute);
+app.use('/book',bookRoute);
 
-//Complete book list
-app.get("/findall", (req, res) => {
-  Post.find({}, function (err, Post) {
-    res.send(Post);
-  });
-});
-
-//Posting an element
-app.post("/", (req, res) => {
-  const user = new Post({
-    bookName: req.body.bookName,
-    authorName: req.body.authorName,
-    image: req.body.image,
-  });
-  user.save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-//finding a specific element
-app.get("/search/:id", async (req, res) => {
-  try {
-    const result = await Post.findById(req.params.id, {
-      useFindAndModify: false,
-    });
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(400).send({ error: "No such file" });
-  }
-});
-//updating a single data in database
-app.get("/update/:id", async (req, res) => {
-  try {
-    const result1 = await Post.findByIdAndUpdate(
-      req.params.id,
-      { bookName: "Rohit Gautam Book" },
-      { useFindAndModify: false }
-    );
-    res.status(200).send({ message: "DataBase Updated" });
-  } catch (error) {
-    res.status(400).send({ error: "Error Updating" });
-  }
-});
-
-//Delete a specific element
-app.get("/delete/:id", async (req, res) => {
-  const book = req.params.bookName;
-  try {
-    const result = await Post.findByIdAndRemove(req.params.id, {
-      useFindAndModify: false,
-    });
-    res.status(200).send({ message: "Item Deleted" });
-  } catch (error) {
-    res.status(400).send({ error: "Error Deleting" });
-  }
-});
-
-//404 Page Not Found Error
-app.use((req, res, next) => {
-  res.status(404).send({ error: "Page Not Found" });
-});
-
-//Connection to local mongoDB
+//Connecton to my local database
 connect(
-  "mongodb://localhost:27017/books",
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  () => {
-    try {
-      console.log("Connected to database");
-    } catch (error) {
-      console.log(error);
+    process.env.DB_CONNECTION,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => {
+      try {
+        console.log("Connected to database");
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
-);
+  );
 
-//App listening to port 5000
-app.listen(PORT, () => {
-  console.log(`Server listening at ${PORT} `);
-});
+//app listening at port 3000
+app.listen(port,()=>{
+    console.log(`Server listening at ${port}`);
+})
